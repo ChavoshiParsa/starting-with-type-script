a6cd690464d309370ea9ffbc5d1e87a565c7d0f1
Your Parsa <parypary82@gmail.com>
2023-07-18 01:14:51 +0330

    New Todo Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
+import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
 
 export default function App() {
-  const todos = [new Todo("Learn React"), new Todo("Learn Type Script")];
+  // const todos = [new Todo("Learn React"), new Todo("Learn Type Script")];
+
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodos((prevTodos) => prevTodos.concat(newTodo));
+  };
+
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
