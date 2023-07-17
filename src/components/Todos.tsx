66c73b9d3661d91665b16021608256ab904da8e2
Your Parsa <parypary82@gmail.com>
2023-07-18 00:15:39 +0330

    seprate Todos and make TodoItem component and configure it with types

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Todo from "../models/todo";
+import TodoItem from "./TodoItem";
 
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
       {props.items.map((item) => (
-        <li key={item.id}>{item.text}</li>
+        <TodoItem key={item.id} text={item.text} />
       ))}
     </ul>
   );
